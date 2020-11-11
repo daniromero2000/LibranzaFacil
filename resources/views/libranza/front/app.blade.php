8d6d94aa49c457fe138b0e0a1486bbcc8d79a33c
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-11-11 10:56:30 -0500

    home

--- resources/views/libranza/front/app.blade.php
+++ resources/views/libranza/front/app.blade.php
@@ -9,14 +9,11 @@
         <meta name="author" content="Oportunidadaes Libranza">
         <title>Libranza Fácil - Oportunidades Libranza</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="{{asset('modules/libranza/public/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/bootstrap/bootstrap.min.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('modules/libranza/public/css/main.min.css')}}" />
-        <link rel="stylesheet" type="text/css" href="{{asset('modules/libranza/public/css/slick.css"')}}" />
-        <link rel="stylesheet" type="text/css" href="{{asset('modules/libranza/public/css/whatsapp.min.css')}}" />
-        <link rel="stylesheet" type="text/css" href="{{asset('modules/libranza/public/css/slick-theme.css')}}" />
-        <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
-        <script src="https://kit.fontawesome.com/c1313463c5.js" crossorigin="anonymous"></script>
         <link rel="icon" type="image/png" href="resources/assets/oportunidadesServicios.ico" />
+        @yield('styles')
+
         <meta name="description" content="Ingresa a Libranzafacil.com y Obten tu crédito por Libranza para pensionados,
     docentes y militares muy fácil, solicítalo Online." />
         <meta property="og:type" content="Website" />
@@ -28,12 +25,9 @@
         <meta property="og:image" content="https://libranzafacil.com/resources/assets/Portada_OGV.jpg" />
         <meta property="og:url" content="https://libranzafacil.com/" />
         <meta property="og:site_name" content="LibranzaFacil.com" />
-        <!-- Global site tag (gtag.js) - Google Ads: 781153823 -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-781153823"></script>
-        @yield('css')
-
         @yield('og')
 
+        @include('libranza.front.layouts.tracking_labels')
         @yield('tracing')
         <script>
             window.dataLayer = window.dataLayer || [];
@@ -49,12 +43,8 @@
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164894259-1"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
+        function gtag() {dataLayer.push(arguments);}
         gtag('js', new Date());
-
         gtag('config', 'UA-164894259-1');
         </script>
         <script>
@@ -66,18 +56,7 @@
             p.parentNode.insertBefore(m, p);
         })(window, document, 'script', 'https://u.heatmap.it/log.js');
         </script>
-        <style>
-            #emailAlert {
-                display: block;
-                padding: 0px 15px;
-                font-size: 11px;
-                font-weight: bold;
-                position: absolute;
-                display: none;
-            }
 
-        </style>
-        @yield('styles')
     </head>
 
     <body>
@@ -86,67 +65,13 @@
         @include('libranza.front.footer')
 
     </body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="{{asset('modules/libranza/public/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('modules/libranza/public/js/slick.min.js')}}"></script>
-    <script src="{{asset('modules/libranza/public/js/jqueryMigrate.min.js')}}"></script>
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script type="text/javascript" src="{{asset('modules/libranza/public/js/whatsapp.min.js')}}"> </script>
+
+    <script src="{{asset('js/bootstrap/jquery.slim.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap/bootstrap.bundle.min.js')}}"></script>
     <script>
 
     </script>
-    <script type="text/javascript">
-        // 1. This code loads the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
 
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-            height: '100%',
-            width: '100%',
-            playerVars: {
-                loop: 1,
-                controls: 1,
-                showinfo: 1,
-                autohide: 1,
-                modestbranding: 1,
-                autoplay: 0,
-                vq: 'hd720'
-            },
-            videoId: 'QQdp5gF7LAw',
-            events: {
-                'onStateChange': onPlayerStateChange
-            }
-        });
-    }
-
-    // 3. The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-        event.target.playVideo();
-    }
-
-    var done = false;
-    </script>
-
-    <script type="text/javascript">
-        $(function() {
-        $('#WAButton').floatingWhatsApp({
-            phone: '573138701355', //WhatsApp Business phone number
-            headerTitle: 'Chatea con nosotros por WhatsApp !', //Popup Title
-            popupMessage: 'Hola, como podemos ayudarte?', //Popup Message
-            showPopup: true, //Enables popup display
-            buttonImage: '<img src="whatsapp.svg" />', //Button Image
-            // headerColor: 'crimson', //Custom header color
-            //backgroundColor: 'crimson', //Custom background button color
-            position: "right" //Position: left | right
-
-        });
-    });
-    </script>
     <script type="text/javascript">
         var email = document.getElementById("email").value;
     var emailConfirm = document.getElementById("emailConfirm").value;
@@ -158,46 +83,6 @@
         document.getElementById("emailAlert").style.display = "none";
     }
     </script>
-    <script type="text/javascript">
-        // 1. This code loads the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    function stopVideo() {
-        player.stopVideo();
-    }
-    </script>
-    <script type="application/ld+json">
-        {
-        "@context": "https://schema.org/",
-        "@type": "WebSite",
-        "name": "Libranza Facil",
-        "url": "https://libranzafacil.com/",
-        "sameAs": [
-            "https://www.facebook.com/almacenes.oportunidades/",
-            "https://www.instagram.com/almacenes.oportunidades/"
-        ],
-        "potentialAction": {
-            "@type": "SearchAction",
-            "target": "https://libranzafacil.com/{search_term_string}",
-            "query-input": "required name=search_term_string"
-        }
-    }
-    </script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-
-    gtag('config', 'UA-164894259-1');
-    </script>
-
     @yield('scripts')
 
 </html>
---
+++ resources/views/libranza/front/layouts/tracking_labels.blade.php
@@ -0,0 +1,19 @@
+<!-- Global site tag (gtag.js) - Google Ads: 781153823 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-781153823"></script>
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org/",
+        "@type": "WebSite",
+        "name": "Libranza Facil",
+        "url": "https://libranzafacil.com/",
+        "sameAs": [
+            "https://www.facebook.com/almacenes.oportunidades/",
+            "https://www.instagram.com/almacenes.oportunidades/"
+        ],
+        "potentialAction": {
+            "@type": "SearchAction",
+            "target": "https://libranzafacil.com/{search_term_string}",
+            "query-input": "required name=search_term_string"
+        }
+    }
+</script>
