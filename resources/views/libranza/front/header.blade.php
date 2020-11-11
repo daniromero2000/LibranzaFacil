514cc56c1f962379e6cc29e64cdf85f9d7a97385
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-11-11 11:41:20 -0500

    home

--- resources/views/libranza/front/header.blade.php
+++ resources/views/libranza/front/header.blade.php
@@ -1,4 +1,5 @@
 <header>
     <img src="{{asset('modules/libranza/public/img/logos/logo-libranzafacil.png')}}" alt="" class="img-card-general">
     <img src="{{asset('modules/libranza/public/img/logos/logo-oportunidades.png')}}" alt="" class="img-card-general">
+    <img src="{{asset('modules/libranza/public/img/icons/50-años.png')}}" alt="" class="img-card-general">
 </header>
