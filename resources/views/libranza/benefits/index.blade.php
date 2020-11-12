6a64e2689f460afd62c598d7e6759012fbece768
Efrain Ignacio Espana Sanchez <efrain.espana@laso.local>
2020-11-12 14:11:01 -0500

    actualizacion modules

---
+++ resources/views/libranza/benefits/index.blade.php
@@ -0,0 +1,55 @@
+@extends('libranza.front.app')
+@section('content')
+
+  <div class="row mb-3">
+    <div class="col-md-6">
+      <img src="modules/libranza/public/img/banners/Rectangulo redondeado.png" class="card-img" alt="...">
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+        <h5 class="card-title">Crédito para <strong>pensionados</strong></h5>
+        <p class="card-text">Se parte de nuestra comunidad donde podrás conocer y adquirir nuestros productos y servicios<strong>(Crédito Libre Inversión, Compra de Cartera y Crédito para Electrodomésticos)</strong>, brindando un Mundo de Oportunidades para ti.</p>        
+      </div>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-md-6">
+     <div>
+     <div class="card-body">
+        <h5 class="card-title">Crédito para <strong>pensionados</strong></h5>
+        <p class="card-text">Se parte de nuestra comunidad donde podrás conocer y adquirir nuestros productos y servicios<strong>(Crédito Libre Inversión, Compra de Cartera y Crédito para Electrodomésticos)</strong>, brindando un Mundo de Oportunidades para ti.</p>        
+      </div>
+     </div>
+    </div>
+    <div class="col-md-6">
+      <img src="modules/libranza/public/img/banners/Rectangulo redondeado.png" class="card-img" alt="...">
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-md-6">
+      <img src="modules/libranza/public/img/banners/Rectangulo redondeado.png" class="card-img" alt="...">
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+        <h5 class="card-title">Crédito para <strong>pensionados</strong></h5>
+        <p class="card-text">Se parte de nuestra comunidad donde podrás conocer y adquirir nuestros productos y servicios<strong>(Crédito Libre Inversión, Compra de Cartera y Crédito para Electrodomésticos)</strong>, brindando un Mundo de Oportunidades para ti.</p>        
+      </div>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-md-6">
+      <div class="card-body">
+        <h5 class="card-title">Crédito para <strong>pensionados</strong></h5>
+        <p class="card-text">Se parte de nuestra comunidad donde podrás conocer y adquirir nuestros productos y servicios<strong>(Crédito Libre Inversión, Compra de Cartera y Crédito para Electrodomésticos)</strong>, brindando un Mundo de Oportunidades para ti.</p>        
+      </div>
+    </div>
+    <div class="col-md-6">
+      <img src="modules/libranza/public/img/banners/Rectangulo redondeado.png" class="card-img" alt="...">
+    </div>
+  </div>
+
+<link rel="stylesheet" type="text/css" href="{{asset('modules/libranza/public/css/benefits/app.min.css')}}" />
+@endsection
