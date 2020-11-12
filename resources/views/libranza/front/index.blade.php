8daf3d6b345e7beee976ffa03c1021519b5c5e22
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-11-12 16:34:08 -0500

    home

--- resources/views/libranza/front/index.blade.php
+++ resources/views/libranza/front/index.blade.php
@@ -33,9 +33,10 @@
 
 <article class="main-text-container">
     <section>
-        <h2>¿Sabes qué es crédito por libranza?</h2>
+        <h2>¿Sabes qué es <b>crédito por libranza</b> ?</h2>
         <p>
-            El crédito por Libranza es un crédito de consumo que incluye un mecanismo de recaudo, en donde el deudor
+            El <span>crédito por Libranza</span> es un crédito de consumo que incluye un mecanismo de recaudo, en donde
+            el deudor
             autoriza a su
             empleador o entidad pagadora para que le realice un descuento de su salario o pensión, con el objetivo de
             que esos
@@ -46,30 +47,52 @@
 
 <article class="container-text-benefits">
     <div>
-        <h2>Beneficios de nuestro crédito</h2>
+        <h2>Beneficios de nuestro <b>crédito</b></h2>
     </div>
     <div class="row">
         <div class="col-4">
-            <p>
-                Estudio y desembolso de crédito para libre inversión, compra de cartera y/o electrodomésticos
-            </p>
+            <div class="container-icon-cards">
+                <img src="{{asset('modules/libranza/public/img/icons/price.png')}}" alt="" class="icon-card">
+                <p>
+                    Estudio y desembolso de crédito para <b>libre inversión, compra de cartera</b> y/o
+                    <b>electrodomésticos</b>
+                </p>
+            </div>
         </div>
         <div class="col-4">
-            <p>Políticas de crédito flexibles para pensionados con reportes en centrales de riesgo</p>
+            <div class="container-icon-cards">
+                <img src="{{asset('modules/libranza/public/img/icons/cards.png')}}" alt="" class="icon-card">
+                <p>Políticas de <b>crédito flexibles para pensionados</b> con reportes en centrales de riesgo</p>
+            </div>
+
         </div>
         <div class="col-4">
-            <p>Atención personalizada y asistida, por nuestro personal a nivel nacional</p>
+            <div class="container-icon-cards">
+                <img src="{{asset('modules/libranza/public/img/icons/call.png')}}" alt="" class="icon-card">
+                <p><b>Atención personalizada y asistida</b>, por nuestro personal a nivel nacional</p>
+            </div>
         </div>
         <div class="col-4">
-            <p>Prestamos hasta los 84 años de edad</p>
+            <div class="container-icon-cards">
+
+                <img src="{{asset('modules/libranza/public/img/icons/age.png')}}" alt="" class="icon-card">
+                <p>Prestamos hasta los <b>84 años de edad</b></p>
+            </div>
         </div>
         <div class="col-4">
-            <p>Descontamos de tu cuenta de pensión para que no te preocupes por fecha de pagos</p>
+            <div class="container-icon-cards">
+
+                <img src="{{asset('modules/libranza/public/img/icons/like.png')}}" alt="" class="icon-card">
+                <p>Descontamos de tu cuenta de pensión para que no te preocupes por fecha de pagos</p>
+            </div>
         </div>
         <div class="col-4">
-            <p>Desde 500 mil hasta 80 millones</p>
+            <div class="container-icon-cards">
+
+                <img src="{{asset('modules/libranza/public/img/icons/cash.png')}}" alt="" class="icon-card">
+                <p>Desde <b>500 mil</b> hasta <b>80 millones</b></p>
+            </div>
         </div>
-    </div>
 </article>
 
 <article class="video-container">
@@ -81,13 +104,15 @@
         </div>
         <div class="col-info">
             <section>
-                <h1>Crédito para pensionados</h1>
+                <h1>Crédito para <b>pensionados</b></h1>
                 <p>
                     Se parte de nuestra comunidad donde podrás conocer y adquirir todos nuestros producto y servicios
-                    (Crédito Libre
-                    Inversión, Compra de Cartera y Crédito para Electrodomésticos), brindando un Mundo de Oportunidades
+                    <b>(Crédito Libre
+                        Inversión, Compra de Cartera y Crédito para Electrodomésticos)</b>, brindando un Mundo de
+                    Oportunidades
                     para ti.
                 </p>
+                <a href="#">Solicita tu crédito</a>
             </section>
         </div>
     </div>
@@ -95,24 +120,36 @@
 
 <section>
     <div class="banner-last">
-        <img src="{{asset('modules/libranza/public/img/banners/banner.png')}}" class="img-fluid"
+        <img src="{{asset('modules/libranza/public/img/banners/banner.png')}}"
             alt="Con-tu-crédito-por-libranza-ahora-es-más-fácil-unificar-tus-deudas">
         <a href="#">Solicita tu crédito</a>
     </div>
 </section>
+
+
+<article>
+    <section class="container-covenants">
+        <h2>Principales convenios</h2>
+
+        <div class="row justify-content-center">
+            <div>Colpensiones</div>
+            <div>BBVA</div>
+            <div>AXA COLPATRIA</div>
+            <div>Colfondos</div>
+            <div>Porvenir</div>
+            <div>Protección</div>
+            <div>MAPFRE</div>
+            <div>POSITIVA</div>
+            <div>Sura</div>
+            <div>Seguros <br> Bolívar</div>
+            <div>Fiduprevisora</div>
+            <div>Seguros alfa s.a</div>
+            <div>Global Seguros</div>
+            <div>FOPEP</div>
+        </div>
+    </section>
+</article>
 {{--
-<div class=" row-reset row contenedor">
-    <div class="col-12 col-sm-12 col-lg-12 col-xl-12 col-reset ">
-        <img class="imgBackground img-general"
-            src="{{asset('modules/libranza/public/resources/assets/ImagenFondo.jpg')}}"
-style="width:100%; margin-top:-1px;" alt="background-libranza-Facil">
-<img class="imgBackground img-movil" src="{{asset('modules/libranza/public/resources/assets/Banner-Cel')}}.png"
-    style="width:100%; margin-top:-1px" alt="background-libranza-Facil">
-</div>
-@include('libranza.front.layouts.form_customers')
-</div>
-@include('libranza.front.layouts.banners.banner_pensionados')
-@include('libranza.front.layouts.banners.banner_benefits')
 @include('libranza.front.layouts.banners.banner_covenants')
 --}}
 @endsection
