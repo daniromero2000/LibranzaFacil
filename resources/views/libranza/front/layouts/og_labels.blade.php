627d7cab86420e4d3028c48ea38b525563c75545
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-12-04 12:16:59 -0500

    SEO

--- resources/views/libranza/front/layouts/og_labels.blade.php
+++ resources/views/libranza/front/layouts/og_labels.blade.php
@@ -6,7 +6,7 @@
 <meta property="og:url" content="https://libranzafacil.com/" />
 <meta property="og:site_name" content="LibranzaFacil.com" />
 
-{{-- 
+
 <!-- Twitter Card -->
 <meta name="twitter:card" content="Libranza Fácil - Oportunidades Libranza">
 <meta name="twitter:creator" content="https://libranzafacil.com/" />
@@ -14,13 +14,4 @@
 <meta name="twitter:title" content="Libranza Fácil - Oportunidades Libranza" />
 <meta name="twitter:description"
     content="Ingresa a Libranzafacil.com y obten tu crédito por Libranza para pensionados, docentes y militares muy fácil, solicítalo Online. Crédito por Libranza para pensionados, docentes y militares, hasta con 120 meses de plazo, aprobamos hasta los 84 años y Somos especialistas en Fondos Privados, Si estas reportado, te damos una segunda Oportunidad." />
-<meta name="twitter:image" content="{{asset('modules/libranza/public/img/logos/logo-libranzafacil.png')}}" /> --}}
-
-
-<meta name="twitter:card" content="Libranza Fácil - Oportunidades Libranza" />
-<meta name="twitter:site" content="https://libranzafacil.com/" />
-<meta name="twitter:creator" content="https://libranzafacil.com/" />
-<meta property="og:url" content="https://libranzafacil.com/" />
-<meta property="og:title" content="Libranza Fácil - Oportunidades Libranza" />
-<meta property="og:description" content="Ingresa a Libranzafacil.com y obten tu crédito por Libranza para pensionados, docentes y militares muy fácil, solicítalo Online. Crédito por Libranza para pensionados, docentes y militares, hasta con 120 meses de plazo, aprobamos hasta los 84 años y Somos especialistas en Fondos Privados, Si estas reportado, te damos una segunda Oportunidad." />
-<meta property="og:image" content="{{asset('modules/libranza/public/img/logos/logo-libranzafacil.png')}}" />
+<meta name="twitter:image" content="{{asset('modules/libranza/public/img/logos/logo-libranzafacil.png')}}" />
