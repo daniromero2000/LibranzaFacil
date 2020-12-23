21361f14c5db395d930b2dfb06b37f72b4ac190b
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-12-23 13:58:33 -0500

    SEO

--- resources/views/libranza/front/layouts/og_labels.blade.php
+++ resources/views/libranza/front/layouts/og_labels.blade.php
@@ -2,7 +2,7 @@
 <meta property="og:title" content="Libranza Facil" />
 <meta property="og:description"
     content="Ingresa a Libranzafacil.com y obten tu crédito por Libranza para pensionados, docentes y militares muy fácil, solicítalo Online. Crédito por Libranza para pensionados, docentes y militares, hasta con 120 meses de plazo, aprobamos hasta los 84 años y Somos especialistas en Fondos Privados, Si estas reportado, te damos una segunda Oportunidad." />
-<meta property="og:image" content="{{ asset('modules/libranza/public/img/banners/banner.png') }}" />
+<meta property="og:image" content="{{ asset('modules/libranza/public/img/banners/banner.jpeg') }}" />
 <meta property="og:url" content="https://libranzafacil.com/" />
 <meta property="og:site_name" content="LibranzaFacil.com" />
 
