d3cfb47871ff50e4a1370bd575a61c1458c6f3ff
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-12-23 13:55:19 -0500

    SEO

--- resources/views/libranza/front/layouts/banners/banner_last.blade.php
+++ resources/views/libranza/front/layouts/banners/banner_last.blade.php
@@ -1,7 +1,1 @@
-<section>
-    <div class="banner-last">
-        <img src="{{asset('modules/libranza/public/img/banners/banner.png')}}"
-            alt="Con-tu-crédito-por-libranza-ahora-es-más-fácil-unificar-tus-deudas">
-        <a href="{{route('form-libranza')}}">Solicita tu crédito</a>
-    </div>
-</section>
+<section> <div class="banner-last"> <img src="{{asset('modules/libranza/public/img/banners/banner.jpeg')}}" alt="Con-tu-crédito-por-libranza-ahora-es-más-fácil-unificar-tus-deudas"> <a href="{{route('form-libranza')}}">Solicita tu crédito</a> </div> </section>
