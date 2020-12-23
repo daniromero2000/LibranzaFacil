c422b94e6538860aae23b6be4c963bc0f3d15954
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-12-23 11:10:34 -0500

    SEO

--- resources/views/libranza/front/layouts/meta.blade.php
+++ resources/views/libranza/front/layouts/meta.blade.php
@@ -1,17 +1,1 @@
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta charset="UTF-8">
-<title>Libranza Fácil - Oportunidades Libranza</title>
-<meta name="author" content="Oportunidadaes Libranza">
-<meta name="language" content="ES">
-<meta name="subject" content="Oportunidadaes Libranza">
-<meta name="copyright" content="Oportunidadaes Libranza">
-<meta name="tags" content="Credito por libranza, Crédito, Libranzas">
-<meta name="keywords" content="Credito por libranza, Crédito, Libranzas">
-<meta name="description"
-    content="Ingresa a Libranzafacil.com y Obten tu crédito por Libranza para pensionados, docentes y militares muy fácil, solicítalo Online." />
-<link href="https://libranzafacil.com/" rel="canonical" />
-<link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('modules/libranza/public/css/app.min.css') }}" />
-<link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta charset="UTF-8"> <title>Libranza Fácil - Oportunidades Libranza</title> <meta name="author" content="Oportunidadaes Libranza"> <meta name="language" content="ES"> <meta name="subject" content="Oportunidadaes Libranza"> <meta name="copyright" content="Oportunidadaes Libranza"> <meta name="tags" content="Credito por libranza, Crédito, Libranzas"> <meta name="keywords" content="Credito por libranza, Crédito, Libranzas"> <meta name="description" content="Ingresa a Libranzafacil.com y Obten tu crédito por Libranza para pensionados, docentes y militares muy fácil, solicítalo Online." /> <link href="https://libranzafacil.com/" rel="canonical" /> <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}"> <link rel="stylesheet" type="text/css" href="{{ asset('modules/libranza/public/css/app.min.css') }}" /> <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}" /> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
