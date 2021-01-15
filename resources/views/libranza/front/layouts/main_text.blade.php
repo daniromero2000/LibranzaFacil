f606d5952ac2aeecdf9a2e4ef6db408d32d0c65b
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2021-01-15 08:22:25 -0500

    API

--- resources/views/libranza/front/layouts/main_text.blade.php
+++ resources/views/libranza/front/layouts/main_text.blade.php
@@ -1,1 +1,9 @@
-<article class="main-text-container"> <section> <h2>¿Sabes qué es <b>crédito por libranza</b> ?</h2> <p> El <span>crédito por Libranza</span> es un crédito de consumo que incluye un mecanismo de recaudo, en donde el deudor autoriza a su empleador o entidad pagadora para que le realice un descuento de su salario o pensión, con el objetivo de que esos dineros descontados, sean destinados al pago de las cuotas del crédito adquirido. </p> </section> </article>
+<article class="main-text-container">
+    <section>
+        <h2>¿Sabes qué es <b>crédito por libranza</b> ?</h2>
+        <p> El <span>crédito por Libranza</span> es un crédito de consumo que incluye un mecanismo de recaudo, en donde
+            el deudor autoriza a su empleador o entidad pagadora para que le realice un descuento de su salario o
+            pensión, con el objetivo de que esos dineros descontados, sean destinados al pago de las cuotas
+            del crédito adquirido. </p>
+    </section>
+</article>
