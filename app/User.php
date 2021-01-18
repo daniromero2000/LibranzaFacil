b387130e93890f7a58c73dcde4be2c3fdb0af114
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2021-01-18 15:00:01 -0500

    API

--- app/User.php
+++ app/User.php
@@ -6,7 +6,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
-use App\OauthAccessToken;
 
 class User extends Authenticatable
 {
@@ -39,7 +38,5 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function tokenExist(){
-        return $this->belongsTo(OauthAccessToken::class);
-    }
+    
 }
