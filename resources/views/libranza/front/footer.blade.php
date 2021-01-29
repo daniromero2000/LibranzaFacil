e84d3ff48dc03c699b4f263c5557efbc255db36c
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2021-01-29 14:40:37 -0500

    Numero de cel

--- resources/views/libranza/front/footer.blade.php
+++ resources/views/libranza/front/footer.blade.php
@@ -1,1 +1,71 @@
-<footer class="footer-lib"> <div class="row mx-0 justify-content-center"> <div class="container-logo-footer"> <img class="logo1" src="{{ asset('modules/libranza/public/img/logos/logo-alternativo-libranzafacil.png') }}" alt=""> </div> <div class="container-logo-footer"> <img class="logo2" src="{{ asset('modules/libranza/public/img/logos/logo-alternativo-oportunidades.png') }}" alt=""> </div> </div> <div class="row text-center container-text-footer justify-content-center"> <div class="col-text-footer"> <div class="text-left"> <ul class="nosotros servicio"> <h5 class="title-foot">Nosotros</h5> <li> <a class="text-foot" href="/codigo-etica" title="Código de ética y buen gobierno corporativo">Código de ética y buen gobierno corporativo</a> </li> <li> <a class="text-foot" href="/quienes-somos" title="Quiénes somos">Quiénes somos</a> </li> <li> <a class="text-foot" href="/proteccion-de-datos-personales" title="Protección de datos personales">Protección de datos personales</a> </li> <li> <a class="text-foot" href="/terminos-y-condiciones" title="Términos y condiciones">Términos y condiciones</a> </li> <li> <a class="text-foot" href="{{route('regulation')}}" title="Reglamentos y condiciones de Credito">Reglamentos y condiciones de Crédito</a> </li> <li> <a class="text-foot" href="/tasas-y-tarifas" title="Tasas y Tarifas">Tasas y Tarifas</a> </li> </ul> </div> </div> <div class="col-text-footer"> <div class="text-left"> <ul class="servicio"> <h5 class="title-foot">Servicio al cliente</h5> <li> <a class="text-foot" href="/por-que-comprar-con-nosotros" class="footer-menuItem" title="Por qué comprar con nosotros">Por qué comprar con nosotros</a> </li> <li> <a class="text-foot" href="/cambios-devoluciones-y-atencion-de-garantias" class="footer-menuItem" title="Cambios , devoluciones y atención de garantías">Cambios, devoluciones y atención de garantías</a> </li> <li> <a class="text-foot" href="http://www.sic.gov.co/proteccion-del-consumidor" target="_blank" class="footer-menuItem" title="Protección al consumidor">Protección al consumidor</a> </li> <li> <a class="text-foot" href="/preguntas-frecuentes" class="footer-menuItem" title="Preguntas Frecuentes">Preguntas Frecuentes</a> </li> <li> <a class="text-foot" href="/indicadores" class="footer-menuItem" title="Preguntas Frecuentes">Indicadores</a> </li> </ul> </div> </div> <div class="col-text-footer last-col-footer"> <div class="col px-0 d-flex servicio"> <div class="row w-100 mx-0"> <div class="col-12 px-0 m-auto py-2"> <div class="row w-100 mx-0"> <div class="container-icon-call "> <img style=" border-radius: 2vw; " src="{{ asset('modules/libranza/public/img/icons/whatsapp.png') }}" class="icon-call2" alt=""> </div> <div class="col pl-2 pr-00"> <p class="text-foot2"><span class="linea">Linea nacional</span><br> <span class="tels"><b>312 2399959</b></span><br> <span class="horario">Lunes a Viernes 8:00 am <br>a 5:00 pm</span> </p> </div> </div> </div> <div class="col pl-11"> <img src=" {{ asset('modules/libranza/public/img/icons/facebook-logo-bold.png') }}" class="icons" onclick="reloadUrl('https://www.facebook.com/almacenes.oportunidades/',2)" alt=""> <img src="{{ asset('modules/libranza/public/img/icons/instagram-logo-bold.png') }}" class="icons ml-2-responsive" onclick="reloadUrl('https://www.instagram.com/almacenes.oportunidades/',2)" alt=""> <span class="ml-2-responsive1">Síguenos</span> </div> </div> </div> </div> </div> </footer>
+<footer class="footer-lib">
+    <div class="row mx-0 justify-content-center">
+        <div class="container-logo-footer"> <img class="logo1"
+                src="{{ asset('modules/libranza/public/img/logos/logo-alternativo-libranzafacil.png') }}" alt=""> </div>
+        <div class="container-logo-footer"> <img class="logo2"
+                src="{{ asset('modules/libranza/public/img/logos/logo-alternativo-oportunidades.png') }}" alt=""> </div>
+    </div>
+    <div class="row text-center container-text-footer justify-content-center">
+        <div class="col-text-footer">
+            <div class="text-left">
+                <ul class="nosotros servicio">
+                    <h5 class="title-foot">Nosotros</h5>
+                    <li> <a class="text-foot" href="/codigo-etica"
+                            title="Código de ética y buen gobierno corporativo">Código de ética y buen gobierno
+                            corporativo</a> </li>
+                    <li> <a class="text-foot" href="/quienes-somos" title="Quiénes somos">Quiénes somos</a> </li>
+                    <li> <a class="text-foot" href="/proteccion-de-datos-personales"
+                            title="Protección de datos personales">Protección de datos personales</a> </li>
+                    <li> <a class="text-foot" href="/terminos-y-condiciones" title="Términos y condiciones">Términos y
+                            condiciones</a> </li>
+                    <li> <a class="text-foot" href="{{ route('regulation') }}"
+                            title="Reglamentos y condiciones de Credito">Reglamentos y condiciones de Crédito</a> </li>
+                    <li> <a class="text-foot" href="/tasas-y-tarifas" title="Tasas y Tarifas">Tasas y Tarifas</a> </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-text-footer">
+            <div class="text-left">
+                <ul class="servicio">
+                    <h5 class="title-foot">Servicio al cliente</h5>
+                    <li> <a class="text-foot" href="/por-que-comprar-con-nosotros" class="footer-menuItem"
+                            title="Por qué comprar con nosotros">Por qué comprar con nosotros</a> </li>
+                    <li> <a class="text-foot" href="/cambios-devoluciones-y-atencion-de-garantias"
+                            class="footer-menuItem" title="Cambios , devoluciones y atención de garantías">Cambios,
+                            devoluciones y atención de garantías</a> </li>
+                    <li> <a class="text-foot" href="http://www.sic.gov.co/proteccion-del-consumidor" target="_blank"
+                            class="footer-menuItem" title="Protección al consumidor">Protección al consumidor</a> </li>
+                    <li> <a class="text-foot" href="/preguntas-frecuentes" class="footer-menuItem"
+                            title="Preguntas Frecuentes">Preguntas Frecuentes</a> </li>
+                    <li> <a class="text-foot" href="/indicadores" class="footer-menuItem"
+                            title="Preguntas Frecuentes">Indicadores</a> </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-text-footer last-col-footer">
+            <div class="col px-0 d-flex servicio">
+                <div class="row w-100 mx-0">
+                    <div class="col-12 px-0 m-auto py-2">
+                        <div class="row w-100 mx-0">
+                            <div class="container-icon-call "> <img style=" border-radius: 2vw; "
+                                    src="{{ asset('modules/libranza/public/img/icons/whatsapp.png') }}"
+                                    class="icon-call2" alt=""> </div>
+                            <div class="col pl-2 pr-00">
+                                <p class="text-foot2"><span class="linea">Linea nacional</span><br> <span
+                                        class="tels"><b>318 2392113</b></span><br> <span class="horario">Lunes a Viernes
+                                        8:00 am <br>a 5:00 pm</span> </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col pl-11"> <img
+                            src=" {{ asset('modules/libranza/public/img/icons/facebook-logo-bold.png') }}" class="icons"
+                            onclick="reloadUrl('https://www.facebook.com/almacenes.oportunidades/',2)" alt=""> <img
+                            src="{{ asset('modules/libranza/public/img/icons/instagram-logo-bold.png') }}"
+                            class="icons ml-2-responsive"
+                            onclick="reloadUrl('https://www.instagram.com/almacenes.oportunidades/',2)" alt=""> <span
+                            class="ml-2-responsive1">Síguenos</span> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/libranza/front/header.blade.php
+++ resources/views/libranza/front/header.blade.php
@@ -1,1 +1,33 @@
-<header> <div class="row mx-0 justify-content-center container-header"> <div class="first-div-header d-flex"> <img src="{{asset('modules/libranza/public/img/logos/logo-libranzafacil.png')}}" alt="" onclick="reloadUrl('{{route('home')}}',1)" class="m-auto"> </div> <div class="col d-flex px-0 col-header-responsive"> <img src="{{asset('modules/libranza/public/img/logos/logo-oportunidades.png')}}" alt="" onclick="reloadUrl('https://www.oportunidades.com.co/',2)" class="m-auto second-logo"> </div> <div class="col d-flex px-0 col-header-responsive"> <img src="{{asset('modules/libranza/public/img/logos/lagobo-grupo.png')}}" alt="" onclick="reloadUrl('https://www.lagobo.com/',2)" class="m-auto second-logo"> </div> <div class="last-div-header pl-0" id="logo-hide"> <img src="{{asset('modules/libranza/public/img/icons/51-años.jpg')}}" alt="" class="logo-yellow-header"> </div> </div> </header> <nav class="navbar navbar-expand navbar-light bg-white"> <div class="mr-auto"> <ul class="navbar-nav"> <li class="nav-item"> <a class="nav-link {{ request()->url() == route('home')  ? 'active' : '' }}"  href="{{route('home')}}">Inicio</a> </li> <li class="nav-item"> <a class="nav-link {{ request()->url() == route('benefit') ? 'active' : '' }}" href="{{route('benefit')}}">Beneficios</a> </li> <li class="nav-item"> <a class="nav-link {{ request()->url() == route('about') ? 'active' : '' }}" href="{{route('about')}}">Nosotros</a> </li> {{-- <li class="nav-item"> <a class="nav-link {{ request()->url() == route('regulation') ? 'active' : '' }}"  href="{{route('regulation')}}">¿como adquirir tu crédito?</a> </li> --}} </ul> </div> <a href="{{route('form-libranza')}}" class="button-nav"> <img src="{{asset('modules/libranza/public/img/icons/peso.png')}}" class="" alt=""> Solicita tu crédito aqui </a> </nav>
+<header>
+    <div class="row mx-0 justify-content-center container-header">
+        <div class="first-div-header d-flex"> <img
+                src="{{ asset('modules/libranza/public/img/logos/logo-libranzafacil.png') }}" alt=""
+                onclick="reloadUrl('{{ route('home') }}',1)" class="m-auto"> </div>
+        <div class="col d-flex px-0 col-header-responsive"> <img
+                src="{{ asset('modules/libranza/public/img/logos/logo-oportunidades.png') }}" alt=""
+                onclick="reloadUrl('https://www.oportunidades.com.co/',2)" class="m-auto second-logo"> </div>
+        <div class="col d-flex px-0 col-header-responsive"> <img
+                src="{{ asset('modules/libranza/public/img/logos/lagobo-grupo.png') }}" alt=""
+                onclick="reloadUrl('https://www.lagobo.com/',2)" class="m-auto second-logo"> </div>
+        <div class="last-div-header pl-0" id="logo-hide"> <img
+                src="{{ asset('modules/libranza/public/img/icons/51-años.jpg') }}" alt="" class="logo-yellow-header">
+        </div>
+    </div>
+</header>
+<nav class="navbar navbar-expand navbar-light bg-white">
+    <div class="mr-auto">
+        <ul class="navbar-nav">
+            <li class="nav-item"> <a class="nav-link {{ request()->url() == route('home') ? 'active' : '' }}"
+                    href="{{ route('home') }}">Inicio</a> </li>
+            <li class="nav-item"> <a class="nav-link {{ request()->url() == route('benefit') ? 'active' : '' }}"
+                    href="{{ route('benefit') }}">Beneficios</a> </li>
+            <li class="nav-item"> <a class="nav-link {{ request()->url() == route('about') ? 'active' : '' }}"
+                    href="{{ route('about') }}">Nosotros</a> </li> {{-- <li
+                class="nav-item"> <a class="nav-link {{ request()->url() == route('regulation') ? 'active' : '' }}"
+                    href="{{ route('regulation') }}">¿como adquirir tu crédito?</a> </li>
+            --}}
+        </ul>
+    </div> <a href="{{ route('form-libranza') }}" class="button-nav"> <img
+            src="{{ asset('modules/libranza/public/img/icons/peso.png') }}" class="" alt=""> Solicita tu crédito aqui
+    </a>
+</nav>
